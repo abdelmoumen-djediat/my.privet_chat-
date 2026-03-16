a6a844fa6351c9b2bb0ba11baf5f63b60abf1463
abdelmoumen-djediat <limonialqlf@gmail.com>
2026-03-16 09:03:12 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+// ChatHub configuration file.
+//
+// Edit these values after you create a Firebase project.
+// 1. Go to https://console.firebase.google.com
+// 2. Create a project (or use an existing one)
+// 3. Add a Web app (</>) and copy the config values.
+// 4. Paste them below.
+
+window.CHATHUB_CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyBQRDCwsa9m_T3NPUTLddA23y4qaGqQsKA",
+    authDomain: "chat-mi-6e4cf.firebaseapp.com",
+    databaseURL: "https://chat-mi-6e4cf-default-rtdb.firebaseio.com",
+    projectId: "chat-mi-6e4cf",
+    storageBucket: "chat-mi-6e4cf.firebasestorage.app",
+    messagingSenderId: "28189585451",
+    appId: "1:28189585451:web:87cec9c525918ff6faba6e",
+  },
+};
